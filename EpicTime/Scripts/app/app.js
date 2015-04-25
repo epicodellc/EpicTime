dbdfcdef3c0cffdfe9435328a48a010721d1523e
epicodellc <epicodellc@gmail.com>
2015-04-25 01:33:13 -0500

    added views

--- EpicTime/Scripts/app/app.js
+++ EpicTime/Scripts/app/app.js
@@ -7,27 +7,17 @@ app.config(function ($routeProvider) {
 
     $routeProvider.when("/home", {
         controller: "homeController",
-        templateUrl: "/app/views/home.html"
+        templateUrl: "scripts/app/views/home.html"
     });
 
     $routeProvider.when("/employeeAdmin", {
         controller: "employeeCtrl",
-        templateUrl: "/app/views/employeeAdmin.html"
+        templateUrl: "scripts/app/views/employeeAdmin.html"
     });
 
-    $routeProvider.when("/refresh", {
-        controller: "refreshController",
-        templateUrl: "/app/views/refresh.html"
-    });
-
-    $routeProvider.when("/tokens", {
-        controller: "tokensManagerController",
-        templateUrl: "/app/views/tokens.html"
-    });
-
-    $routeProvider.when("/associate", {
-        controller: "associateController",
-        templateUrl: "/app/views/associate.html"
+    $routeProvider.when("/clientAdmin", {
+        controller: "employeeCtrl",
+        templateUrl: "scripts/app/views/clientAdmin.html"
     });
 
     $routeProvider.otherwise({ redirectTo: "/home" });
