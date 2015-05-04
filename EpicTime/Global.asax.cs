82867fcb8369b07ff360c9adc5d004c260a14235
epicodellc <epicodellc@gmail.com>
2015-05-04 18:24:32 -0500

    global

--- EpicTime/Global.asax.cs
+++ EpicTime/Global.asax.cs
@@ -37,7 +37,7 @@ namespace EpicTime
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            //GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
         }
     }
 }
